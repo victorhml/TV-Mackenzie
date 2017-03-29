53b628e3f03745a59d610756b3c45bdfad73d3a3
Joaquim Pessoa Filho <joaquimp@gmail.com>
2017-03-29 20:53:55 -0300

    Removido arquivos desnecessários e ajuste no projeto e algumas imagens, pois estavam com tamanho diferente por causa da versão

--- TV Mackenzie/AppDelegate.swift
+++ TV Mackenzie/AppDelegate.swift
@@ -97,7 +97,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
     // MARK: TVApplicationControllerDelegate
     
     func appController(_ appController: TVApplicationController, didFinishLaunching options: [String: Any]?) {
-        print("\(#function) invoked with options: \(options)")
+        print("\(#function) invoked with options: \(String(describing: options))")
     }
     
     func appController(_ appController: TVApplicationController, didFail error: Error) {
@@ -113,7 +113,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
     }
     
     func appController(_ appController: TVApplicationController, didStop options: [String: Any]?) {
-        print("\(#function) invoked with options: \(options)")
+        print("\(#function) invoked with options: \(String(describing: options))")
     }
 }
 
